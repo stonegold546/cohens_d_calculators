2cadb28bb702a6128ec015a14cb327a9c316d158
James <stonegold546@gmail.com>
2017-03-05 17:00:21 -0500

    Why?!

--- config/config_env.rb
+++
@@ -1,8 +0,0 @@
-config_env :development do
-  set 'PYTHON_URL', 'http://localhost:5000'
-end
-
-config_env :production do
-  set 'MSG_KEY', 'tpFSEbUqJLUFnf5mpCiG2Le4-9vy-osUKaI99jUMwWs='
-  set 'PYTHON_URL', 'https://python-worker.herokuapp.com'
-end
