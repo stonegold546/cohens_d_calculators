8a854c1d519b8ace958ce16c10316490edc88c7e
James <stonegold546@gmail.com>
2017-03-05 16:54:41 -0500

    Config_Rake

---
+++ Rakefile
@@ -0,0 +1,2 @@
+require 'config_env/rake_tasks'
+ConfigEnv.init("#{__dir__}/config/config_env.rb")
--- config/config_env.rb
+++ config/config_env.rb
@@ -1,3 +1,8 @@
+config_env :development do
+  set 'PYTHON_URL', 'http://localhost:9292'
+end
+
 config_env :production do
   set 'MSG_KEY', 'x63_ONYy95W4IzC7ll9DwZmSH8lSuGfNad4cOZe5V-4='
+  set 'PYTHON_URL', 'https://python-worker.herokuapp.com'
 end
