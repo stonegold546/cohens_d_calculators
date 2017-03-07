107f5f10725763cd821ec7f292c8b92b79fd5bb4
James <stonegold546@gmail.com>
2017-03-06 19:04:56 -0500

    Using Python for HLM

--- services/hlm/icc.rb
+++ services/hlm/icc.rb
@@ -14,10 +14,21 @@ class Icc
     num_s = "Clusters: #{@clusters.uniq.count}; "\
       "Cases: #{@values.count} (including missing) units; Method: "\
       "#{which_method} for estimate and variance components, ANOVA for CI"
-    icc_calc = calc_icc_aov
-    icc_calc = calc_icc_ml(icc_calc) unless @method == 'ANOVA'
+    icc_calc = calc_icc
     icc_calc[:inputs] = num_s
     Oj.dump icc_calc
+  end
+
+  def calc_icc
+    response = HTTParty.post "#{ENV['PYTHON_URL']}/icc", body: {
+      x: @clusters, y: @values, method: which_method
+    }.to_json, headers: {
+      'Content-Type' => 'application/json'
+    }
+    response = Oj.load response.body
+    { icc_est: response['ICC'], lower: response['LowerCI'],
+      upper: response['UpperCI'], n: response['N'], k: response['k'],
+      varw: response['varw'], vara: response['vara'] }
   end
 
   def which_method
@@ -25,41 +36,5 @@ class Icc
       'ANOVA' => 'ANOVA', 'TRUE' => TRUE, 'FALSE' => FALSE
     }
     method[@method]
-  end
-
-  def calc_icc_aov
-    response = HTTParty.post URL_ICC_ANOVA, body: {
-      'x' => @clusters.to_s, 'y' => @values.to_s
-    }
-    result = Oj.load response.body
-    create_result_aov(result)
-  end
-
-  def create_result_aov(data)
-    {
-      icc_est: data['ICC'][0], lower: data['LowerCI'][0],
-      upper: data['UpperCI'][0], n: data['N'][0], k: data['k'][0],
-      varw: data['varw'][0], vara: data['vara'][0]
-    }
-  end
-
-  def calc_icc_ml(icc_calc)
-    response = HTTParty.post "#{ENV['PYTHON_URL']}/icc", body: {
-      x: @clusters, y: @values, method: which_method
-    }.to_json, headers: {
-      'Content-Type' => 'application/json'
-    }
-    variances = {}
-    variances[:var_b] = response[0]
-    variances[:var_w] = response[1]
-    create_result_ml(icc_calc, variances)
-  end
-
-  def create_result_ml(icc_calc, variances)
-    icc = variances[:var_b] / (variances[:var_b] + variances[:var_w])
-    icc_calc[:icc_est] = icc
-    icc_calc[:vara] = variances[:var_b]
-    icc_calc[:varw] = variances[:var_w]
-    icc_calc
   end
 end
