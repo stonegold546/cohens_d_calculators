50d575b00a37757bce7e322ae499e3779fed7f4a
James <stonegold546@gmail.com>
2017-03-11 11:19:57 -0500

    One sample works!

--- services/t_tests/cohen_d_z.rb
+++ services/t_tests/cohen_d_z.rb
@@ -6,6 +6,7 @@ class CohenDz
     @pop_mean = one_sample.pop_mean.to_f
     @sample_sd = one_sample.sample_sd.to_f
     @n = one_sample.n.to_f
+    @conf_int = one_sample.conf_int.to_f / 100
     @t = calc_t
   end
 
@@ -13,7 +14,7 @@ class CohenDz
     cohen_dz = (@sample_mean - @pop_mean) / @sample_sd
     result = { cohen_dz: cohen_dz, lower_limit: '', upper_limit: '',
                inputs: @inputs }
-    return Oj.dump result if @t.zero? || @n.zero?
+    return Oj.dump result if @t.zero? || @n.zero? || @conf_int.zero?
     npci = non_par_conf_int
     result = { cohen_dz: cohen_dz, lower_limit: npci[:lower],
                upper_limit: npci[:upper], inputs: @inputs }
--- values/t_tests/one_sample.rb
+++ values/t_tests/one_sample.rb
@@ -10,9 +10,12 @@ class OneSample
   attribute :pop_mean, Float
   attribute :sample_sd, Float
   attribute :n, Float
+  attribute :conf_int, Float
 
   validates_numericality_of :sample_mean
   validates_numericality_of :pop_mean
   validates_numericality_of :sample_sd, greater_than: 0
   validates_numericality_of :n, greater_than: 1, allow_nil: true
+  validates_numericality_of :conf_int, greater_than: 0, less_than: 100,
+                                       allow_nil: true
 end
