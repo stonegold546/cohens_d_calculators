cd85fa130326e2643dc79d3f9729a33d3e64fb91
James <stonegold546@gmail.com>
2017-03-11 11:52:08 -0500

    Paired works!

--- values/anova/anova.rb
+++ values/anova/anova.rb
@@ -9,8 +9,14 @@ class Anova
   attribute :f, Float
   attribute :df_effect, Float
   attribute :df_error, Float
+  attribute :conf_int, Float
 
   validates_numericality_of :f, greater_than: 0
   validates_numericality_of :df_effect, greater_than: 0
   validates_numericality_of :df_error, greater_than: 1
+  validates_numericality_of :conf_int, greater_than: 0, less_than: 100
+
+  def confidence_interval
+    conf_int / 100.0
+  end
 end
